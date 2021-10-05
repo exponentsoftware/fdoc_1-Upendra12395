07271a2fb04b48945ac02464aa674aa70657c23d
Upendra12395 <ch95upendra@gmail.com>
2021-10-05 14:13:25 +0530

    problem 1b

---
+++ day1_1b.js
@@ -0,0 +1,5 @@
+// Write a function called cleanText. The function takes raw text as a parameter and returns the clean text.
+const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
+
+const cleanText = (rawText) => rawText.replace(/[,;#%&@!$']/g, "");
+console.log(cleanText(sentence)); //`I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`
